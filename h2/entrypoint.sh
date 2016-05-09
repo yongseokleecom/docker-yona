74dd3f6f723d86018e52fb3969a71dbccb4b40f3
yongseoklee <yongseokleecom@gmail.com>
2016-05-09 18:36:40 +0900

    H2 용 도커파일 추가

---
+++ h2/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+export YONA_HOME=/yona
+export PID_FILE=$YONA_HOME/RUNNING_PID
+
+# Check JAVA Options
+if [ "x$JAVA_OPTS" != "x" ]; then
+    echo "[INFO] JAVA OPTIONS: $JAVA_OPTS"
+    export _JAVA_OPTIONS=$JAVA_OPTS
+fi
+
+# Remove if PID file exists.
+if [ -f $PID_FILE ]; then
+    echo "[INFO] Remove pid file: $PID_FILE"
+    rm $PID_FILE
+fi
+
+/bin/bash /opt/yona/bin/yona
